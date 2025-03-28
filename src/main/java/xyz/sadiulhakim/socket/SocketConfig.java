478e7b4e7c7a5b19e70255f2a5736289dcba816b
Sadiul Hakim <sadiulhakim@gmail.com>
2025-03-28 22:19:42 +0600

    Task Scheduler

--- src/main/java/xyz/sadiulhakim/socket/SocketConfig.java
+++ src/main/java/xyz/sadiulhakim/socket/SocketConfig.java
@@ -3,6 +3,7 @@ package xyz.sadiulhakim.socket;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.SimpleAsyncTaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -30,12 +31,15 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
 
         // Use a shared scheduled executor
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setThreadFactory(Thread.ofVirtual().factory());
-        scheduler.initialize();
+//        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+//        scheduler.setThreadFactory(Thread.ofVirtual().factory());
+//        scheduler.initialize();
+
+        SimpleAsyncTaskScheduler taskScheduler = new SimpleAsyncTaskScheduler();
+        taskScheduler.setVirtualThreads(true);
 
         registry.enableSimpleBroker("/queue")
-                .setTaskScheduler(scheduler);
+                .setTaskScheduler(taskScheduler);
 
         registry.setApplicationDestinationPrefixes(appPrefix);
         registry.setUserDestinationPrefix(userPrefix);
